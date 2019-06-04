f0ba21bb6bb3349c260bdcc6bba971f36c9c8962
Layton Whiteley <layton.w@smart-host.com>
2019-06-04 20:50:25 +0200

    Add max warnings cli option

--- app.js
+++ app.js
@@ -2,16 +2,24 @@
 
 const CLIEngine = require('eslint').CLIEngine;
 const path = require('path');
+const yargs = require('yargs');
 const write = require('write');
 const debug = require('debug')('eslint-output');
 const rc = require('./config');
 
 const cwd = path.resolve(process.cwd());
+const { maxWarnings } = yargs.argv;
 
-const cli = new CLIEngine(Object.assign({
-  envs: ['browser', 'mocha'],
-  useEslintrc: true,
-}, rc.cliEngineConfig, { cwd }));
+const cli = new CLIEngine(
+  Object.assign(
+    {
+      envs: ['browser', 'mocha'],
+      useEslintrc: true,
+    },
+    rc.cliEngineConfig,
+    { cwd },
+  ),
+);
 
 const report = cli.executeOnFiles(rc.files || ['.']);
 
@@ -21,12 +29,18 @@ const outputs = {
   },
   file(output, format) {
     if (!format.path) {
-      return debug(`a 'path' prop is required for this format (${format.name}), please specify and run again`);
+      return debug(
+        `a 'path' prop is required for this format (${
+          format.name
+        }), please specify and run again`,
+      );
     }
     try {
       return write.sync(path.resolve(cwd, format.path), output);
     } catch (e) {
-      return debug(`Could not write file for eslint format: ${format.name} - ${e.message}`);
+      return debug(
+        `Could not write file for eslint format: ${format.name} - ${e.message}`,
+      );
     }
   },
 };
@@ -35,10 +49,10 @@ let formats = rc.formats;
 
 if (!Array.isArray(rc.formats)) {
   formats = [{ name: 'stylish' }];
-  debug('using default format \'stylish\'');
+  debug("using default format 'stylish'");
 }
 
-formats.forEach((format) => {
+formats.forEach(format => {
   const formatter = cli.getFormatter(format.name);
   if (formatter) {
     const outputMethod = outputs[format.output] || outputs.console;
@@ -48,7 +62,10 @@ formats.forEach((format) => {
   }
 });
 
-if (report.errorCount > 0) {
+if (
+  report.errorCount > 0 ||
+  (maxWarnings !== undefined && report.warningCount > maxWarnings)
+) {
   process.exitCode = 1;
+  debug('exited with code: 1');
 }
-
