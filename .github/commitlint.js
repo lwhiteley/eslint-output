0acb54fd5d8109ac80b2189c72f3949294fb888d
Layton Whiteley <layton.w@smart-host.com>
2024-10-26 16:47:41 +0200

    ci: move from travisci to github actions (#22)

---
+++ .github/commitlint.js
@@ -0,0 +1,19 @@
+module.exports = {
+  rules: {
+    'body-leading-blank': [2, 'always'],
+    'footer-leading-blank': [1, 'always'],
+    'header-max-length': [2, 'always', 100],
+    'subject-case': [
+      2,
+      'never',
+      ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
+    ],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'scope-case': [2, 'always', 'lower-case'],
+    'duplicate-type-subject-start': [2, 'always'],
+    'duplicate-commit-type': [2, 'always'],
+  },
+};
